f3db2a949c3270b2d098bdfed21476bd2894e51c
Sumedh Ambapkar <asumedh@amazon.com>
2022-06-20 11:23:33 -0400

    Changed Tag

--- workload/terraform/data.tf
+++ workload/terraform/data.tf
@@ -2,11 +2,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-data "aws_ec2_transit_gateway" "tgw" {
-    filter {
-      name   = "managed_by"
-      values = ["AFT"]
-    }
-}
+data "aws_ec2_transit_gateway" "tgw" {}
 
 data "aws_vpc_ipam_pool" "ipam_pool" {}
